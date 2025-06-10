e479bcf55007c4a112ed1f34310bfff6e4699da5
Dylan Aubrey <dylan.aubrey@bjss.com>
2025-06-10 20:28:34 +0100

    fix(envs): guard against envs not being set on globalThis

--- packages/envs/src/getEnv.ts
+++ packages/envs/src/getEnv.ts
@@ -1,3 +1,4 @@
-// Want ability to return literal string type is user requires.
-// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-export const getEnv = <T extends string>(key: string): T | undefined => globalThis.envs[key] as T | undefined;
+export const getEnv = <T extends string>(key: string): T | undefined =>
+  // Want ability to return literal string type is user requires.
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  ('envs' in globalThis ? globalThis.envs[key] : undefined) as T | undefined;
