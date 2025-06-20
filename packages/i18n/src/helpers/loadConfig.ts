c24b27a586e0facb4df52ad4ae371247a3ba0e3e
miami-man <badbatchdev@gmail.com>
2025-06-20 11:23:10 +0100

    fix(i18n): config file path resolves wrong

--- packages/i18n/src/helpers/loadConfig.ts
+++ packages/i18n/src/helpers/loadConfig.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 import { CONFIG_FILENAME } from '#constants.ts';
 import { type MacrosConfig } from '#types.ts';
 
@@ -8,7 +9,9 @@ export const loadConfig = (): Partial<MacrosConfig> => {
   try {
     // JSON.parse returns an any time
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    config = JSON.parse(readFileSync(CONFIG_FILENAME, { encoding: 'utf8' })) as Partial<MacrosConfig>;
+    config = JSON.parse(
+      readFileSync(resolve(process.cwd(), CONFIG_FILENAME), { encoding: 'utf8' }),
+    ) as Partial<MacrosConfig>;
   } catch {
     // no catch
   }
