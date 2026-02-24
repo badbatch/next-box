59bd6e8315615937e88245a15853e91d808aafe5
miami-man <badbatchdev@gmail.com>
2026-02-24 00:22:35 +0000

    fix(envs): remove redundant code

--- packages/envs/src/setWorkerEnvs.ts
+++ packages/envs/src/setWorkerEnvs.ts
@@ -10,11 +10,5 @@ export const setWorkerEnvs = (callback?: () => void): void => {
     }
   };
 
-  // Condition is necessary
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (globalThis.env) {
-    callback?.();
-  }
-
   globalThis.addEventListener('message', onMessage);
 };
