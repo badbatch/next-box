f1649b57e5f115a05ae88467413fd8af06052171
miami-man <badbatchdev@gmail.com>
2026-08-29 12:10:27 +0100

    fix(breadcrumb): update BreadcrumbRouteRule generics

--- packages/breadcrumb/src/types.ts
+++ packages/breadcrumb/src/types.ts
@@ -7,14 +7,12 @@ export type BreadcrumbItem = {
   label: string;
 };
 
-export interface BreadcrumbRouteRule<T extends Record<string, unknown> = Record<string, unknown>> {
+export interface BreadcrumbRouteRule<T extends object = object> {
   regex: string;
   resolve: (captured: T) => Promisable<string>;
   searchParmExclusions?: string[];
 }
 
-export type CreateRouteRules<T extends Record<string, unknown> = Record<string, unknown>> = (
-  options: T,
-) => BreadcrumbRouteRule[];
+export type CreateRouteRules<T extends object = object> = (options: T) => BreadcrumbRouteRule[];
 
 export type OnBreadcrumbLinkClick = (breadcrumbEntry: BreadcrumbItem, event: MouseEvent<HTMLAnchorElement>) => void;
